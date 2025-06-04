93a28b792165c0e20843d47ba03d709b9643773a
Jerrit Jaison <95865876+JerritJaison@users.noreply.github.com>
2025-06-04 23:17:33 +0530

    Update settings.py

--- diabetic_retinopathy/settings.py
+++ diabetic_retinopathy/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,6 +124,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # if you have a static folder
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
